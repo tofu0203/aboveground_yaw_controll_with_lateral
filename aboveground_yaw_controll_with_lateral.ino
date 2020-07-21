01ae15591713c942586124c4243321b3f8c19565
OkitaTomoya <special1168@gmail.com>
2020-07-21 14:08:57 +0900

    add servo command function

--- aboveground_yaw_controll_with_lateral.ino
+++ aboveground_yaw_controll_with_lateral.ino
@@ -58,6 +58,76 @@ int limit_servo_command_value(float value)
 	}
 }
 
+//servo_command=lateral1_thrust_to_servo_command(thrust)--------------------------------------
+const float lateral1_param1[4] = {1.42000573e+03, -1.27692693e+01, -4.62346562e-01, -2.79686070e-02};
+const float lateral1_param2[4] = {1410.018664, -47.59048111, -7.52998927, 24.85048449};
+const float lateral1_param3[4] = {1391.83293, -14.2056781, 0.863936724, -0.0510286803};
+int lateral1_thrust_to_servo_command(float thrust)
+{
+	int command;
+	float x1 = thrust;
+	float x2 = thrust * x1;
+	float x3 = thrust * x2;
+	if (thrust < -0.7448)
+	{
+		command = int(lateral1_param1[0] + lateral1_param1[1] * x1 + lateral1_param1[2] * x2 + lateral1_param1[3] * x3);
+	}
+	else if (-0.7448 <= thrust && thrust <= 0.882)
+	{
+		command = int(lateral1_param2[0] + lateral1_param2[1] * x1 + lateral1_param2[2] * x2 + lateral1_param2[3] * x3);
+	}
+	else if (0.882 < thrust)
+	{
+		command = int(lateral1_param3[0] + lateral1_param3[1] * x1 + lateral1_param3[2] * x2 + lateral1_param3[3] * x3);
+	}
+
+	//limmitter
+	if (command > 1550)
+	{
+		command = 1550;
+	}
+	else if (command < 1230)
+	{
+		command = 1230;
+	}
+	return command;
+}
+
+const float lateral2_param1[4] = {1.33485718e+03, -1.26789886e+01, -5.94512376e-01, -3.30347612e-02};
+const float lateral2_param2[4] = {1326.84944302, -62.67557326, 7.02881259, 106.85549092};
+const float lateral2_param3[4] = {1.31021169e+03, -1.40550417e+01, 6.30551538e-01, -3.50974486e-02};
+int lateral2_thrust_to_servo_command(float thrust)
+{
+	int command;
+	float x1 = thrust;
+	float x2 = thrust * x1;
+	float x3 = thrust * x2;
+	if (thrust < -0.668)
+	{
+		command = int(lateral2_param1[0] + lateral2_param1[1] * x1 + lateral2_param1[2] * x2 + lateral2_param1[3] * x3);
+	}
+	else if (-0.668 <= thrust && thrust <= 0.393)
+	{
+		command = int(lateral2_param2[0] + lateral2_param2[1] * x1 + lateral2_param2[2] * x2 + lateral2_param2[3] * x3);
+	}
+	else if (0.393 < thrust)
+	{
+		command = int(lateral2_param3[0] + lateral2_param3[1] * x1 + lateral2_param3[2] * x2 + lateral2_param3[3] * x3);
+	}
+
+	//limmitter
+	if (command > 1450)
+	{
+		command = 1450;
+	}
+	else if (command < 1180)
+	{
+		command = 1180;
+	}
+	return command;
+}
+//-------------------------------------------------------------------------------------
+
 //コールバック関数
 //自重分のスロットル+yaw制御分にリミットをかけモータに出力
 void yawCallback(const std_msgs::Float32 &command_value)
